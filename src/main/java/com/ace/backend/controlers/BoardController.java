10df3a925323ade68f38c6ffb9b99237920a8651
unknown <arthurcrowder@att.net>
2022-03-02 13:33:47 -0600

    adding mapping for id

--- src/main/java/com/ace/backend/controlers/BoardController.java
+++ src/main/java/com/ace/backend/controlers/BoardController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
- 
+import com.ace.backend.exceptions.ResourceNotFoundException;
 import com.ace.backend.models.board;
 import com.ace.backend.repositories.BoardRepository;
 
@@ -29,9 +29,16 @@ public class BoardController {
 		return boardRepo.findAll();
 	}
 	
-	
-	
-	
+	@GetMapping("boardtype/{id}")
+		public ResponseEntity<board> getboardById(@PathVariable int id){
+			board board = boardRepo.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Boardtype not found")); 
+					return ResponseEntity.ok(board);
+			
+		}
+
+		
+		
 	
 }
 	
