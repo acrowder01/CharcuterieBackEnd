70dddfcd3c91032470359b1c02275b44391bad8d
unknown <arthurcrowder@att.net>
2022-03-02 18:13:47 -0600

    updating delete api

--- src/main/java/com/ace/backend/controlers/BoardController.java
+++ src/main/java/com/ace/backend/controlers/BoardController.java
@@ -3,10 +3,14 @@ package com.ace.backend.controlers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,6 +49,25 @@ public class BoardController {
 				}
 				return boardRepo.findBydescription(description);
 			}
+			
+			@PostMapping("addboard")
+			public board newboard(@RequestBody board board) {
+				return boardRepo.save(board);
+			}
+			
+			@DeleteMapping("boardtype/{id}")
+			public ResponseEntity<String> deleteboard(@PathVariable int id) {
+				boardRepo.findById(id)
+				
+				.orElseThrow(() -> new ResourceNotFoundException("Student not found."));
+				
+				String message =  "Board has been deleted.";
+				
+				boardRepo.deleteById(id);
+				return new ResponseEntity<>(message, HttpStatus.OK);
+			}
+			
+			
 
 			
 	
